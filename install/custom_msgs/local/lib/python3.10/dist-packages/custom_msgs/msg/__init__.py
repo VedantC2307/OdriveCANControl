ca231f6b5b7c7d3d448a147e19d01bdbdfa2e67b
Vedant <vedantchoudhary07@gmail.com>
2025-01-06 15:22:30 -0700

    .

---
+++ install/custom_msgs/local/lib/python3.10/dist-packages/custom_msgs/msg/__init__.py
@@ -0,0 +1,1 @@
+from custom_msgs.msg._motion_state import MotionState  # noqa: F401
---
+++ install/custom_msgs/share/custom_msgs/cmake/rosidl_cmake-extras.cmake
@@ -0,0 +1,4 @@
+# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
+
+set(custom_msgs_IDL_FILES "msg/MotionState.idl")
+set(custom_msgs_INTERFACE_FILES "msg/MotionState.msg")
