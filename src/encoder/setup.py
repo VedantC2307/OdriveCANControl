4619b5e738d9f20eb36cac5b77fcc71fdc3c8818
Vedant <vedantchoudhary07@gmail.com>
2025-01-10 11:21:24 -0700

    Updates packages and launch file!

--- src/encoder/setup.py
+++ src/encoder/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'encoder_read_node = encoder.encoder_read_node:main',
+            'encoder_node = encoder.encoder_read_node:main',
         ],
     },
 )
