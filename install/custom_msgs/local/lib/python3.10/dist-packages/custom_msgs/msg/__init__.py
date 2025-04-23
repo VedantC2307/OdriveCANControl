6d0cb0a8d98fcf6f41a32b2ead8c20a57e9ddbd9
Vedant <vedantchoudhary16@gmail.com>
2025-04-23 15:06:26 -0700

    Remove build and log directories from git tracking

--- install/custom_msgs/local/lib/python3.10/dist-packages/custom_msgs/msg/__init__.py
+++
@@ -1,3 +0,0 @@
-from custom_msgs.msg._friction_comp import FrictionComp  # noqa: F401
-from custom_msgs.msg._impedance_torque import ImpedanceTorque  # noqa: F401
-from custom_msgs.msg._motion_state import MotionState  # noqa: F401
